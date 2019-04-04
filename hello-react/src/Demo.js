b7a7b03929eff2d0591f713e68245c4992f5d54c
nguyenconghau <nguyenconghau188@gmail.com>
2019-04-05 00:29:27 +0700

    add first reactjs

---
+++ hello-react/src/Demo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Demo extends Component {
+  render() {
+    return (
+      <div>
+          <h1>
+            Hello ReactJS
+          </h1>
+          <p>My first ReactJS application</p>
+      </div>
+    );
+  }
+}
+
+export default Demo;
