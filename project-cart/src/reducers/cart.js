7c047bbe1abeacb89ad7060f49d4b9227c77967f
nguyenconghau <nguyenconghau188@gmail.com>
2019-11-04 21:58:19 +0700

    save cart to localstorage

--- project-cart/src/reducers/cart.js
+++ project-cart/src/reducers/cart.js
@@ -1,32 +1,32 @@
 import * as types from '../constants/ActionType';
 var data = JSON.parse(localStorage.getItem('CART'));
-// var initialState = data ? data : [];
-var initialState = [
-    {
-        product: {
-            id: 1,
-            name: 'IPhone Xs',
-            image: 'https://cdn.fptshop.com.vn/Uploads/Originals/2018/10/11/636748771945393060_iPhone-Xs-Max-gold.png',
-            description: 'Sản phẩm do Apple sản xuất',
-            price: 1000,
-            inventory: 10,
-            rating: 5
-        },
-        quantity: 5
-    },
-    {
-        product: {
-            id: 2,
-            name: 'Samsung Galaxy S10',
-            image: 'https://cdn.fptshop.com.vn/Uploads/Originals/2019/2/21/636863643187455627_ss-galaxy-s10-trang-1.png',
-            description: 'Sản phẩm do Samsung sản xuất',
-            price: 900,
-            inventory: 15, 
-            rating: 5
-        }, 
-        quantity: 3
-    }
-];
+var initialState = data ? data : [];
+// var initialState = [
+//     {
+//         product: {
+//             id: 1,
+//             name: 'IPhone Xs',
+//             image: 'https://cdn.fptshop.com.vn/Uploads/Originals/2018/10/11/636748771945393060_iPhone-Xs-Max-gold.png',
+//             description: 'Sản phẩm do Apple sản xuất',
+//             price: 1000,
+//             inventory: 10,
+//             rating: 5
+//         },
+//         quantity: 5
+//     },
+//     {
+//         product: {
+//             id: 2,
+//             name: 'Samsung Galaxy S10',
+//             image: 'https://cdn.fptshop.com.vn/Uploads/Originals/2019/2/21/636863643187455627_ss-galaxy-s10-trang-1.png',
+//             description: 'Sản phẩm do Samsung sản xuất',
+//             price: 900,
+//             inventory: 15, 
+//             rating: 5
+//         }, 
+//         quantity: 3
+//     }
+// ];
 
 const cart = (state = initialState, action) => {
     var { product, quantity } = action;
@@ -42,6 +42,7 @@ const cart = (state = initialState, action) => {
                     quantity
                 });
             }
+            localStorage.setItem('CART', JSON.stringify(state));
             return [...state];
     
         default:
